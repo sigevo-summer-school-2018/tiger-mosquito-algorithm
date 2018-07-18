3330632543df2d94cabafcc2c56e5cdc4a1f99c3
Hugo Daniel Monzón Maldonado <hugo91@gmail.com>
2018-07-18 13:28:48 +0900

    Added license text to every file closes #6

--- code/selection/tiger_mosquito_selection.py
+++ code/selection/tiger_mosquito_selection.py
@@ -1,3 +1,23 @@
+#
+#	This file is part of the Tiger Mosquito Algorithm - TMA
+#	It implements the selection scheme
+#
+#	TMA is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	(at your option) any later version.
+#
+#	TMA is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+#	You should have received a copy of the GNU General Public License
+#	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+#	Author: Shota Saito
+
+
 from operator import attrgetter
 from deap.tools.selection import selRandom
 
